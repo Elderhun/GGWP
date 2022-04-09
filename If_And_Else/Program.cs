7f2ea991ff5317f06354668c22296a5f43b59a92
Elderhun <kiselovigor970@gmail.comm>
2022-04-10 01:27:42 +0300

    Hello Tanya

---
+++ If_And_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Write a user name");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "tanya")
+{
+    Console.WriteLine("Yo Tanya!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
